75f439d1968defab8fb69c6985b4b1f7ed5035e7
Sergio Palomino Guevara <sergiopalominoguevara@sergios-mbp.mynetworksettings.com>
2022-11-09 20:02:14 -0500

    this is my first commit on cojo branch

--- src/practicareview/rata.java
+++ src/practicareview/rata.java
@@ -5,6 +5,9 @@ public class rata {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("this is my first creation in cojo branch");
+		
+		
 	}
 
 }
